02dbc4b26d56ae93fea3e0a1b0df55d2a4afdbe3
allx4461 <137797438+allx4461@users.noreply.github.com>
2026-03-09 22:14:17 +0300

    orthography refined. nothing sp

--- main.cpp
+++ main.cpp
@@ -10,58 +10,14 @@
 #include "entity.h"
 #include "bubble.h"
 
-// int main(){
-//     srand(time(0));
-//     World world(140, 38);
-//     Canvas canvas(world.width(), world.height());
-//     std::unique_ptr<fish5> entity1 = std::make_unique<fish5>(world.width(), world.height());
-//     world.addEntity(std::move(entity1));
-//     std::unique_ptr<fish6> entity2 = std::make_unique<fish6>(world.width(), world.height());
-//     world.addEntity(std::move(entity2));
-//     std::unique_ptr<fish7> entity3 = std::make_unique<fish7>(world.width(), world.height());
-//     world.addEntity(std::move(entity3));
-//     fish7* entity3_ptr = entity3.get();
-//     srand(time(0));
-//     std::unique_ptr<fish2> entity4 = std::make_unique<fish2>(world.width(), world.height());
-//     world.addEntity(std::move(entity4));
-//     std::unique_ptr<weed1> entity5 = std::make_unique<weed1>(world.width(), world.height());
-//     world.addEntity(std::move(entity5));
-//     std::cout << world.size() << std::endl;
-//     std::cout << "\033[?25l";
-//     for (int i=0; i<20; ++i){
-//         std::unique_ptr<Bubble> bubble = std::make_unique<Bubble>(world.width(), world.height());
-//         world.addEntity(std::move(bubble));
-//         std::cout << "\033[H"; 
-//         canvas.clear();
-//         world.update();
-//         world.render(canvas);
-//         canvas.present();
-//         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//     }
-//     world.removeEntity(entity3_ptr);
-//     std::cout << "REMOOOOOOOOOOVE" << std::endl;
-//     for (int i=0; i<20; ++i){
-//         std::unique_ptr<Bubble> bubble = std::make_unique<Bubble>(world.width(), world.height());
-//         world.addEntity(std::move(bubble));
-//         std::cout << "\033[H"; 
-//         canvas.clear();
-//         world.update();
-//         world.render(canvas);
-//         canvas.present();
-//         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//     }
-//     std::cout << "\033[?25h";
-//     return 0;
-// }
-
 std::string chooseEntity(){
     std::string f;
     bool flag_s = true;
-    std::cout << "You may choose one of this:" << std::endl;
-    std::cout << "MiniShark | JellyFish | mediumFish | MiniFish | ManyFish | Seahorse | Weed | 42" << std::endl;
+    std::cout << "choose from these:" << std::endl;
+    std::cout << "small shark -1 | jellyfish -2 | fish -3 | small fish -4 | fishes -5 | seahorse -6 | seaweed -7 | 42" << std::endl;
     std::getline(std::cin, f);
     while (flag_s){      
-        if (f=="MiniShark" || f=="JellyFish" || f=="mediumFish" || f=="MiniFish" || f=="ManyFish" || f=="Seahorse" || f=="Weed" || f=="42"){
+        if (f=="1" || f=="2" || f=="3" || f=="4" || f=="5" || f=="6" || f=="7" || f=="42"){
             flag_s = false;
         }else{
             std::cout << "enter correct command pls" << std::endl;
@@ -72,28 +28,28 @@ std::string chooseEntity(){
 }
 
 std::unique_ptr<Entity> createFish(std::string s, int width, int height){
-    if (s=="MiniShark"){
+    if (s=="1"){
         std::unique_ptr<fish1> entity = std::make_unique<fish1>(width, height);
         return entity;
-    }else if(s=="MiniFish"){
+    }else if(s=="4"){
         std::unique_ptr<fish2> entity = std::make_unique<fish2>(width, height);
         return entity;
-    }else if(s=="mediumFish"){
+    }else if(s=="3"){
         std::unique_ptr<fish3> entity = std::make_unique<fish3>(width, height);
         return entity;
-    }else if(s=="ManyFish"){
+    }else if(s=="5"){
         std::unique_ptr<fish4> entity = std::make_unique<fish4>(width, height);
         return entity;
-    }else if(s=="JellyFish"){
+    }else if(s=="2"){
         std::unique_ptr<fish5> entity = std::make_unique<fish5>(width, height);
         return entity;
-    }else if(s=="Seahorse"){
+    }else if(s=="6"){
         std::unique_ptr<fish6> entity = std::make_unique<fish6>(width, height);
         return entity;
     }else if(s=="42"){
         std::unique_ptr<fish7> entity = std::make_unique<fish7>(width, height);
         return entity;
-    }else if(s=="Weed"){
+    }else if(s=="7"){
         std::unique_ptr<weed1> entity = std::make_unique<weed1>(width, height);
         return entity;
     }else{
@@ -107,12 +63,12 @@ int main(){
     World world(140, 38);
     Canvas canvas(world.width(), world.height());
     srand(time(0));
-    std::cout << "Welkome to bulbul aquarium ^^" << std::endl;
-    std::cout << "How much ticks do you want?" << std::endl;
+    std::cout << "== aquarium ==" << std::endl;
+    std::cout << "ticks?" << std::endl;
     int tik;
     std::cin >> tik;
     std::cin.ignore();
-    std::cout << "Let's add your first entity to aquarium!" << std::endl;
+    std::cout << "add entity" << std::endl;
     std::string f = chooseEntity();
     auto entity = createFish(f, world.width(), world.height());
     world.addEntity(std::move(entity));
@@ -130,16 +86,16 @@ int main(){
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
         std::cout << "\033[?25h";
-        std::cout << "What do you wanto to do?(choose)" << std::endl;
-        std::cout << "stop | addEntity | playWorld" << std::endl;
+        std::cout << "choose" << std::endl;
+        std::cout << "stop | add | play" << std::endl;
         std::string command;
         std::getline(std::cin, command);
         bool enterFlag = true;
         while (enterFlag){
-            if (command=="stop" || command=="addEntity" || command=="playWorld"){
+            if (command=="stop" || command=="add" || command=="play"){
                 enterFlag=false;
             }else{
-                std::cout << "wtf normal command please :3" << std::endl;
+                std::cout << "try again" << std::endl;
                 std::getline(std::cin, command);
             }
         }
@@ -149,11 +105,11 @@ int main(){
                 flag = false;
                 comF = false;
             }
-            else if (command=="addEntity"){
+            else if (command=="add"){
                 std::string comm = chooseEntity();
                 world.addEntity(createFish(comm, world.width(), world.height()));
                 comF = false;
-            }else if (command=="playWorld"){
+            }else if (command=="play"){
                 std::cout << "\033[?25l";
                 for (int i=0; i<100; ++i){
                     std::unique_ptr<Bubble> bubble = std::make_unique<Bubble>(world.width(), world.height());
@@ -168,7 +124,7 @@ int main(){
                 comF = false;
                 std::cout << "\033[?25h";
             }else{
-                std::cout << "wtf normal command please :3" << std::endl;
+                std::cout << "try again" << std::endl;
                 std::getline(std::cin, command);
             }
         }
